c8a1da98954fbbd9b407097b28fcb9b608a8fa2e
Parksugeun <107028245+Parksugeun@users.noreply.github.com>
2023-01-12 20:00:58 +0900

    JavaScript

--- javascript/D-Day-counter/script.js
+++ javascript/D-Day-counter/script.js
@@ -63,8 +63,16 @@ const counterMaker = function () {
     sec: document.getElementById("sec"),
   };
 
+  const timeKeys = Object.keys(remainingObj);
+  const docKeys = Object.keys(documentObj);
+
+  for (let i = 0; i < timeKeys.length; i = i + 1) {
+    documentObj[docKeys[i]].textContent = remainingObj[timeKeys[i]];
+  }
+  /*
   documentObj["days"].textContent = remainingObj["remainingDate"];
   documentObj["hours"].textContent = remainingObj["remainingHours"];
   documentObj["min"].textContent = remainingObj["remainingMin"];
   documentObj["sec"].textContent = remainingObj["remainingSec"];
+  */
 };
