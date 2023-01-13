60c4449a725b67d40dda1a9cfa933f9d9b1cd1cd
Parksugeun <107028245+Parksugeun@users.noreply.github.com>
2023-01-13 20:00:23 +0900

    JavaScript

--- javascript/D-Day-counter/script.js
+++ javascript/D-Day-counter/script.js
@@ -1,16 +1,8 @@
-const obj = {
-  name: "Peter",
-  age: 25,
-};
-if (obj.name === "Jason" && obj.age >= 25) {
-  console.log("안녕!" + obj.name + "너의나이는" + obj.age);
-} else {
-  console.log("you are not a member!");
-}
 const messageContainer = document.querySelector("#d-day-message");
 const container = document.querySelector("#d-day-container");
+const intervalIdArr = [];
 
-//container.style.display = "none";
+container.style.display = "none";
 messageContainer.innerHTML = "<h3>D-Day를 입력해 주세요.</h3>";
 
 const dateFormMaker = function () {
@@ -21,58 +13,60 @@ const dateFormMaker = function () {
   //const dateFormat = inputYear + '-' + inputMonth + '-' + inputDate
   const dateFormat = `${inputYear}-${inputMonth}-${inputDate}`; // ` 백틱에서는 문자열안에서 바로 변수 데이터를 만들 수 있다.
   return dateFormat;
-  //console.log(inputYear,inputMonth,inputDate);
 };
 const counterMaker = function () {
   const targetDateInput = dateFormMaker();
-  console.log(targetDateInput);
   const nowDate = new Date();
   const targetDate = new Date(targetDateInput).setHours(0, 0, 0, 0);
   const remaining = (targetDate - nowDate) / 1000;
-  // 만약, remaining이 0 이라면, 타이머가 종료 되었습니다. 출력
   // 수도코드(한글로 코드를 작성하는 것)
 
   if (remaining <= 0) {
+    // 만약, remaining이 0 이라면, 타이머가 종료 되었습니다. 출력
+    container.style.display = "none";
     messageContainer.innerHTML = "<h3>타이머가 종료되었습니다.</h3>";
+    messageContainer.style.display = "flex";
+    setClearInterval();
+    return;
   } else if (isNaN(remaining)) {
     // 만약, 잘못된 날짜가 들어왔다면, 유효한 시간대가 아닙니다. 출력
+    container.style.display = "none";
     messageContainer.innerHTML = "<h3>유효한 시간대가 아닙니다.</h3>";
+    messageContainer.style.display = "flex";
+    setClearInterval();
+    return;
   }
-  /*
-  const remainingDate = Math.floor(remaining / 3600 / 24);
-  const remainingHours = Math.floor(remaining / 3600) % 24;
-  const remainingMin = Math.floor(remaining / 60) % 60;
-  const remainingSec = Math.floor(remaining) % 60;
-*/
+
   const remainingObj = {
     remainingDate: Math.floor(remaining / 3600 / 24),
     remainingHours: Math.floor(remaining / 3600) % 24,
     remainingMin: Math.floor(remaining / 60) % 60,
     remainingSec: Math.floor(remaining) % 60,
   };
-  /*
-  const days = document.getElementById("days");
-  const hours = document.getElementById("hours");
-  const min = document.getElementById("min");
-  const sec = document.getElementById("sec");
-*/
-  const documentObj = {
-    days: document.getElementById("days"),
-    hours: document.getElementById("hours"),
-    min: document.getElementById("min"),
-    sec: document.getElementById("sec"),
-  };
 
+  const documentArr = ["days", "hours", "min", "sec"];
   const timeKeys = Object.keys(remainingObj);
-  const docKeys = Object.keys(documentObj);
+  // timeKeys는 배열이고 ['remainingDate', remaingHours, ... 가 들어있음]
 
-  for (let i = 0; i < timeKeys.length; i = i + 1) {
-    documentObj[docKeys[i]].textContent = remainingObj[timeKeys[i]];
+  let i = 0;
+  for (let tag of documentArr) {
+    document.getElementById(tag).textContent = remainingObj[timeKeys];
+    i++;
   }
-  /*
-  documentObj["days"].textContent = remainingObj["remainingDate"];
-  documentObj["hours"].textContent = remainingObj["remainingHours"];
-  documentObj["min"].textContent = remainingObj["remainingMin"];
-  documentObj["sec"].textContent = remainingObj["remainingSec"];
-  */
+};
+
+const starter = function () {
+  container.style.display = "flex";
+  messageContainer.style.display = "none";
+  counterMaker();
+  const intervalId = setInterval(counterMaker, 1000);
+  intervalIdArr.push(intervalId);
+};
+
+const setClearInterval = function () {
+  container.style.display = "none";
+  messageContainer.innerHTML = "<h3>D-Day를 입력해 주세요.</h3>";
+  for (let i = 0; i < intervalIdArr.length; i++) {
+    clearInterval(intervalIdArr[i]);
+  }
 };
